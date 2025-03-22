1b81169fe2d643b4fe0cb19b2be1ffb2208840e1
MaxMeyberg <mmeyberg@stanford.edu>
2025-03-22 12:25:47 -0700

    update

--- backend/API-services/apify.py
+++ backend/API-services/apify.py
@@ -1,19 +1,22 @@
 from apify_client import ApifyClient
 from dotenv import load_dotenv
 
-# Initialize the ApifyClient with your API token
-client = ApifyClient("APIFY_API_TOKEN")
 
-# Prepare the Actor input
-run_input = { "profileUrls": [
-        "https://www.linkedin.com/in/williamhgates",
-        "http://www.linkedin.com/in/jeannie-wyrick-b4760710a",
-    ] }
 
-# Run the Actor and wait for it to finish
-run = client.actor("2SyF0bVxmgGr8IVCZ").call(run_input=run_input)
+def APIFY_LinkedIn_WebScrape():
+    # Initialize the ApifyClient with your API token
+    client = ApifyClient("APIFY_API_TOKEN")
 
-# Fetch and print Actor results from the run's dataset (if there are any)
-for item in client.dataset(run["defaultDatasetId"]).iterate_items():
-    print(item)
-    
+    # Prepare the Actor input
+    run_input = { "profileUrls": [
+            "https://www.linkedin.com/in/williamhgates",
+            "http://www.linkedin.com/in/jeannie-wyrick-b4760710a",
+        ] }
+
+    # Run the Actor and wait for it to finish
+    run = client.actor("2SyF0bVxmgGr8IVCZ").call(run_input=run_input)
+
+    # Fetch and print Actor results from the run's dataset (if there are any)
+    for item in client.dataset(run["defaultDatasetId"]).iterate_items():
+        print(item)
+        
