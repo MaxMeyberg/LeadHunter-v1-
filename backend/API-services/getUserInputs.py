ee6f2bfa6c37790f636b038af73f5530010ad370
MaxMeyberg <mmeyberg@stanford.edu>
2025-03-22 12:20:30 -0700

    next

--- backend/API-services/getUserInputs.py
+++ backend/API-services/getUserInputs.py
@@ -1,1 +1,11 @@
 #TODO: we take in the url from linkedin here and return an email
+
+
+"""
+Input: url from linkedin
+
+Output: their emails
+"""
+def getEmail_via_Linkedin(url: str) -> list[str]:
+    
+    return []
